2e48b871f32a9cf62e0f4a376a3ebfa3c98c0c0e
“MatsumuraSaki” <massaki3156@gmail.com>
2023-10-10 00:24:32 +0900

    データベース設定（blog・userモデルを関連づけ、コントローラ作成）

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_09_150518) do
+ActiveRecord::Schema.define(version: 2023_10_09_151857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "blogs", "users"
 end
