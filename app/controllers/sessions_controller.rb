4a6297747941033bf3c450ca08b75514f65ef7a0
“MatsumuraSaki” <massaki3156@gmail.com>
2023-10-11 16:46:49 +0900

    ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   def new
   end
 
@@ -14,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
